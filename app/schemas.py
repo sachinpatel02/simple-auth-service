3369250528289e7f80b89d051ffcd7e2c7d4ba37
sachinpatel02 <sachinp1613@gmail.com>
2026-02-27 14:54:26 +0530

    🧩 Pydantic models to validate Requests and Responses

---
+++ app/schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
+import uuid
+from datetime import datetime
+
+#---------------------------------------------------------------
+# Request Schemas
+#---------------------------------------------------------------
+
+class UserCreate(BaseModel):
+    """Schema for creating a new user."""
+    email: EmailStr
+    password: str= Field(min_length = 8, max_length = 64, 
+                         description = "Password must be between 8 and 64 characters long.")
+
+class UserLogin(BaseModel):
+    """Schema for user login."""
+    email: EmailStr
+    password: str
+
+class RefreshRequest(BaseModel):
+    """Schema for refreshing access token."""
+    refresh_token: str
+
+#---------------------------------------------------------------
+# Response Schemas
+#---------------------------------------------------------------
+
+class UserResponse(BaseModel):
+    """Schema for returning user data. (No password)"""
+    id: uuid.UUID
+    email: EmailStr
+    is_active: bool
+    role : str | None
+    created_at: datetime
+    updated_at: datetime
+    model_config = ConfigDict(from_attributes = True)
+
+class TokenResponse(BaseModel):
+    """Schema for returning access and refresh tokens."""
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+    
